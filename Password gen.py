0e61d58b0ed6b18d0f13efc56e39567d84584a3d
Hannes Johansen <20053215@tafe.wa.edu.au>
2021-03-03 13:11:47 +0800

    Password_gen1.0

---
+++ Password gen.py
@@ -0,0 +1,15 @@
+from random import *
+from string import *
+
+Q = input("Generate Password? ")
+
+password = ""
+
+pchars = ascii_letters + digits
+
+if Q == "yes" or Q == "Yes":
+    for i in range(10):
+        password += choice(pchars)
+
+print(password)
+        
